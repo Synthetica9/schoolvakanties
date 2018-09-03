7d528435d91cf83ef876162e7320cabd064925ad
Patrick Hilhorst <git@hilhorst.be>
2018-09-04 00:51:45 +0200

    Added static server

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from urllib.parse import urljoin
 import re
 from icalendar import Event, Calendar
 import git
+import json
+import os
 
 ENTRY_URL = 'https://www.rijksoverheid.nl/onderwerpen/schoolvakanties/overzicht-schoolvakanties-per-schooljaar'
 PARSER = 'html.parser'
@@ -78,17 +80,26 @@ def generate_calendars(entry_url=ENTRY_URL):
     urls = data_urls(entry_url)
     for url in urls:
         for region, events in parse_data(url).items():
-            calendar = calendars.setdefault(region.replace(' ', '') + '.ical', Calendar())
+            calendar = calendars.setdefault(region.replace(' ', ''), Calendar())
             calendar['prodid'] = PRODID
             calendar['version'] = ICAL_VERSION
             for event in events:
                 calendar.add_component(event)
     return calendars
 
+def static_root():
+    with open("static.json") as f:
+        j = json.load(f)
+        return j['root']
+
 def main():
+    root = static_root()
+    os.makedirs(root, exist_ok=True)
     for region, calendar in generate_calendars().items():
-        with open(region, 'wb') as f:
+        path = os.path.join(root, region + '.ical')
+        with open(path, 'wb') as f:
             f.write(calendar.to_ical())
 
 if __name__ == '__main__':
+    print(static_root())
     main()
